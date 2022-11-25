66d317d09b6976d98c67e088016099f1514ca9cb
Marina-Chuhlib <107684222+Marina-Chuhlib@users.noreply.github.com>
2022-11-25 13:54:43 +0000

    Deploying to gh-pages from @ Marina-Chuhlib/goit-js-hw-09@8c31f5e3bdcac74316b167ab7b04b79a7b40c457 🚀

---
+++ 03-promises.46decef4.js
@@ -0,0 +1,2 @@
+document.querySelector("button");const e=document.querySelector(".form");document.getElementsByName("delay"),document.getElementsByName("step"),document.getElementsByName("amount");e.addEventListener("input",(e=>{Number(e.target.value)})),console.log(formData);
+//# sourceMappingURL=03-promises.46decef4.js.map
